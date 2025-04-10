d8e5600ed446a68d2f3893ba0c6d0ce95ac09de0
Snehalchavan12 <sneh.more88@gmail.com>
2025-04-10 15:41:34 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,11 @@
+import math
+number = 20
+square_root = math.sqrt(number)
+print(f"The square root of {number} is {square_root}")
+
+log_value = math.log(number, 2)  # Logarithm base 2
+print(f"The logarithm of {number} is {log_value}")
+
+
+sin_value = math.sin(number)
+print("Sin:", sin_value)
